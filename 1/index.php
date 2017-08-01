5b7ae3b56e674ea6d407cd69571762e7f3f6aa11
rovogit <tequi11az@mail.ru>
2017-08-01 19:48:55 +0500

    Домашнее задание 1

---
+++ 1/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Урок 1: Быстрый старт: выражения и переменные, типы.</title>
+</head>
+<body>
+    <div id="main" class="container">
+        <h2>
+            <b>Задание 1. "Установить локальный сервер"</b>
+            <b class="green">&#10004</b>
+            <br>
+            <b><?php include __DIR__ . '/1.php';?></b>
+        </h2>
+        <h2>
+            <b>Задание 2. "Добавить офф. php мануал в закладки"</b>
+            <b class="green">&#10004</b>
+            <br>
+            <b><?php include __DIR__ . '/2.php';?></b>
+        </h2>
+        <h2>
+            <b>Задание 3. "Изучить функцию var_dump($var)"</b>
+            <b class="green">&#10004</b>
+            <br>
+            <b><?php include __DIR__ . '/3.php';?></b>
+        </h2>
+        <h2>
+            <b>Задание 4. "Что является значением выражения присваивания"</b>
+            <b class="green">&#10004</b>
+            <br>
+            <b><?php include __DIR__ . '/4.php';?></b>
+        </h2>
+        <h2>
+            <b>Задание 5. "Изучить логический тип данных (boolean)"</b>
+            <b class="green">&#10004</b>
+            <br>
+            <b><?php include __DIR__ . '/5.php';?></b>
+        </h2>
+    </div>
+</body>
+</html>
